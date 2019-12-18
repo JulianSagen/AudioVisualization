a4879ab80c6c462b06960ee574b8c45fa6439cf8
Julian Sagen <julian.sagen@gmail.com>
2019-12-18 20:42:09 +0100

    Storing state in list of arrays instead of objects for more performance

--- analyzer.js
+++ analyzer.js
@@ -1,65 +1,62 @@
 let mic, fft;
-var i = 0;
 
-let drawState = []
+const drawState = Array(80);
 
 function setup() {
-    createCanvas(1000, 800);
-    noFill();
-  
-    mic = new p5.AudioIn();
-    mic.start();
-    fft = new p5.FFT();
-    fft.setInput(mic);
-    frameRate(100)
-  }
-  
-  function draw() {
-    background(200);
-    drawEngine()
-  }
+  frameRate(5);
 
-  function drawEngine(){
-    drawState = deleteOutdated(drawState)
-    let spectrum = fft.analyze();
-    let filtered = filterAmplitude(spectrum)
-    filtered.forEach(element => spawn(element.freq))
-    drawFrequency()
-    drawState = drawState.map(element => {
-        return {
-        timeAlive: (element.timeAlive - 2),
-        frequency: element.frequency
-    }})
-  }
+  createCanvas(1000, 1600);
+  noFill();
 
-  function deleteOutdated(recorded){
-     return recorded.filter(element => element.timeAlive > 10 )
-  }
+  mic = new p5.AudioIn();
+  mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
+}
 
-  function filterAmplitude(spectrum){
-    return spectrum.reduce((resp,strenght, freq) =>{ 
-        if (strenght > 100){ 
-           resp.push({freq, strenght})
-        } 
-        return resp
-    },[])
-  }
-  function spawn(frequency){
-    drawState.push({
-        timeAlive: 800,
-        frequency: frequency
-    })
-  }
-  function drawFrequency(){
-      drawState.forEach(element => drawDot(element.timeAlive, element.frequency))
-  }
+function draw() {
+  background(200);
+  drawEngine();
+}
 
-  function mouseClicked() {
-    console.log(drawState)
-    let spectrum = fft.analyze();
-    console.log(filterAmplitude(spectrum))
-  }
+function drawEngine() {
+  if (drawState.length > 80) drawState.shift();
+  let spectrum = fft.analyze();
+  if (spectrum) drawState.push(spectrum);
+  drawFrequency();
+}
 
-  function drawDot(x, y) {
-    ellipse(x, y*8, 5, 5);
+function drawFrequency() {
+  if (drawState.length > 1) {
+    drawState.forEach((spectrum, time) => {
+      if (spectrum) {
+        spectrum.forEach((amplitude, frequency) => {
+          if (amplitude > 10) {
+            drawDot(time, frequency, amplitude);
+          }
+        });
+      }
+    });
   }
+}
+
+function drawDot(x, y, amp) {
+  if (amp > 100) {
+    stroke(0, 0, 0);
+  } else if (amp > 70) {
+    stroke(200, 0, 0);
+  } else if (amp > 50) {
+    stroke(0, 255, 0);
+  } else if (amp > 20) {
+    stroke(0, 0, 255);
+  } else {
+    stroke(200, 200, 200);
+  }
+  ellipse(x * 12, y * 10, 5, 5);
+}
+
+function mouseClicked() {
+  console.log(drawState);
+  let spectrum = fft.analyze();
+  console.log(spectrum);
+}
