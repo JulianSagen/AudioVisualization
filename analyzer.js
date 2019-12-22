ef12a2123104a677a0d4caebf9d0339ec7f042cd
Julian <teritry2@gmail.com>
2019-12-22 11:51:42 +0100

    Removed smoothning and added labels for identifying tones in the spectrum

--- analyzer.js
+++ analyzer.js
@@ -7,20 +7,42 @@ function setup() {
 
   createCanvas(1000, 1600);
   noFill();
+  noStroke();
 
   mic = new p5.AudioIn();
   mic.start();
-  fft = new p5.FFT();
+  fft = new p5.FFT(0, 1024);
   fft.setInput(mic);
 }
 
 function draw() {
   background(200);
   drawEngine();
+  drawLabels()
 }
 
+function drawLabels(){
+  fill(50);
+  drawLabel("C3", 65)
+  drawLabel("B3", 105)  
+  drawLabel("C4", 118)
+  drawLabel("D4", 130)
+  drawLabel("G4", 170)
+  drawLabel("C5", 235)
+  drawLabel("G5", 345)
+  drawLabel("C6", 455)
+  drawLabel("G6", 673)
+  drawLabel("C7", 895)
+  drawLabel("G7", 1352)
+}
+
+function drawLabel(label, frequency){
+  text(label,10,frequency)
+}
+
+
 function drawEngine() {
-  if (drawState.length > 80) drawState.shift();
+  if (drawState.length > 50) drawState.shift();
   let spectrum = fft.analyze();
   if (spectrum) drawState.push(spectrum);
   drawFrequency();
@@ -42,15 +64,15 @@ function drawFrequency() {
 
 function drawDot(x, y, amp) {
   if (amp > 100) {
-    stroke(0, 0, 0);
+    fill(0, 0, 0);
   } else if (amp > 70) {
-    stroke(200, 0, 0);
+    fill(200, 0, 0);
   } else if (amp > 50) {
-    stroke(0, 255, 0);
+    fill(0, 255, 0);
   } else if (amp > 20) {
-    stroke(0, 0, 255);
+    fill(0, 0, 255);
   } else {
-    stroke(200, 200, 200);
+    fill(200, 200, 200);
   }
   ellipse(x * 12, y * 10, 5, 5);
 }
